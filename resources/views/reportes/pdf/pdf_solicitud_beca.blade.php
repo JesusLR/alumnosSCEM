7e6fdc43ec8858a6d7cdd265b51778be5eca45d9
Jesus Alberto Lira Romero <jesusliraromero@hotmail.com>
2024-01-05 11:15:24 -0500

    se quitarin las modificaciones anteriores en el metodo body y se implementaron en el metodo page

--- resources/views/reportes/pdf/pdf_solicitud_beca.blade.php
+++ resources/views/reportes/pdf/pdf_solicitud_beca.blade.php
@@ -290,8 +290,8 @@
         width:100%;
         display: block;
         position: relative;
-        margin-left: -30px;
-        margin-right: -30px;
+        /* margin-left: -30px; */
+        /* margin-right: -30px; */
       }
       .row::after {
           content: "";
@@ -416,7 +416,9 @@
         margin-top: 0;
         display: block;
       }
-      @page {
+     @page {
+        margin-left: 0.5cm;
+        margin-right: 0.5cm;
         margin-top: 30px;
         margin-bottom: 30px;
       }
